b91951d932e0c2402714b680c7c84ce5153286f8
nmnthg <77082885+nmnthg@users.noreply.github.com>
2024-05-27 13:50:12 -0400

    Adding categories
    Set up categories and some Sass styling

---
+++ ecommerce-application/src/App.js
@@ -0,0 +1,44 @@
+import './categories.styles.scss';
+
+const App = () => {
+  
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Mens',
+    },
+    {
+      id: 5,
+      title: 'Womens',
+    },
+    
+  ]
+  
+  return (
+    <div className="categories-container">
+      {categories.map(({title, id}) => (
+        <div key={id} className="category-container">
+          <div className="background-image"/>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
