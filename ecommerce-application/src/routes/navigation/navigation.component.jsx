4023c529fdc54dbf960c4eb2aafbfdb6ac38dd74
nmnthg <77082885+nmnthg@users.noreply.github.com>
2024-05-28 19:36:55 -0400

    Nav component and routing
    Added navigation bar and implemented react router for basic routes

---
+++ ecommerce-application/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Link, Outlet } from "react-router-dom"
+import { Fragment } from "react"
+import {ReactComponent as CrwnLogo} from '../../assets/Crown_logo.svg'
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+        <Fragment> 
+            <div className='navigation'>
+                <Link className="logo-container" to='/'>
+                    <CrwnLogo className="logo" />
+                </Link> 
+                
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        <div>SHOP</div>
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+  }
+
+export default Navigation
