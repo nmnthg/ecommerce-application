0d4c61bbac0444cc294f71379f078d7a3336ec59
nmnthg <77082885+nmnthg@users.noreply.github.com>
2024-06-14 18:08:30 -0400

    Card drop down component
    Add card-dropdown component and styling

--- ecommerce-application/src/routes/navigation/navigation.component.jsx
+++ ecommerce-application/src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,14 @@
 import { Link, Outlet } from "react-router-dom";
 import { Fragment, useContext } from "react";
 import {ReactComponent as CrwnLogo} from '../../assets/Crown_logo.svg';
-import './navigation.styles.scss';
-import CartIcon from "../../components/cart-icon/cart-icon.component";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
+import './navigation.styles.scss';
+
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
@@ -33,6 +37,7 @@ const Navigation = () => {
                     }
                     <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </Fragment>
