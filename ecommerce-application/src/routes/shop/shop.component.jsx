d9bd81732434b590f5b9740766f4c72266a94e79
nmnthg <77082885+nmnthg@users.noreply.github.com>
2024-06-20 16:39:56 -0400

    Store and Retrieve data from Firestore
    Add utility functions to firebase.utils.js to store and retrieve data from Firestore. Import the retrieved data and render it into categories and products.

--- ecommerce-application/src/routes/shop/shop.component.jsx
+++ ecommerce-application/src/routes/shop/shop.component.jsx
@@ -1,17 +1,28 @@
 import ProductCard from '../../components/product-card/product-card.component';
-import { useContext } from 'react';
-import { ProductsContext } from '../../contexts/products.context';
+import { useContext, Fragment } from 'react';
+import { CategoriesContext } from '../../contexts/categories.context'
 import './shop.styles.scss';
 
 const Shop = () => {
-    const { products } = useContext(ProductsContext);
+    const { categoriesMap } = useContext(CategoriesContext);
 
     return (
-        <div className='products-container'>
-            {products.map((product) => (
-               <ProductCard key={product.id} product={product}/>
-            ))};
-        </div>
+        <Fragment>
+            {/* Iterate over the keys of the categoriesMap object, returning an 
+                array of category titles. Then we map over that array (going over all
+                the products in each), for each product render a ProductCard component */
+                Object.keys(categoriesMap).map((title) => (
+                    <Fragment key={title}>
+                        <h2>{title}</h2>
+                        <div className='products-container'>
+                            {categoriesMap[title].map((product) => ( 
+                                <ProductCard key={product.id} product={product} />
+                            ))}
+                        </div>
+                    </Fragment>
+                ))
+            }
+        </Fragment>
     );
 }
 
