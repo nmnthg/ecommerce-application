358366d5369ae88a61d66b47a4ab86c1e01f17d9
nmnthg <77082885+nmnthg@users.noreply.github.com>
2024-08-27 17:47:39 -0400

    Resolve Selector Refiring
    Implement reselect caching to category selector to resolve the selector refiring

--- ecommerce-application/src/routes/shop/shop.component.jsx
+++ ecommerce-application/src/routes/shop/shop.component.jsx
@@ -3,15 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action';
 
 const Shop = () => {
-  
+    const dispatch = useDispatch();
+    
     useEffect(() => {
         const getCategoriesMap = async () => {
             const categoryArray = await getCategoriesAndDocuments('categories');
-            // dispatch(categoryArray);
+            dispatch(setCategories(categoryArray));
         };
 
         getCategoriesMap();
