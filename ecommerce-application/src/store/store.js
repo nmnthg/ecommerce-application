45ec780976c3a8ebf5aec584f3596848101532ee
nmnthg <77082885+nmnthg@users.noreply.github.com>
2024-09-03 19:39:58 -0400

    redux devtool

--- ecommerce-application/src/store/store.js
+++ ecommerce-application/src/store/store.js
@@ -1,38 +1,23 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./root-reducer";
+import logger from "redux-logger";
 import { persistStore, persistReducer } from 'redux-persist';
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if(!action.type) {
-        return next (action);
-    } 
-
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-
-    next(action);
-
-    console.log('next state: ', store.getState())
-}
+import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
     key: 'root',
-    storage: storageSession, // use sessionStorage instead of localStorage
+    storage: storage,
     blacklist: ['user'] //we do not want to persist the user because we are getting that value from firebase auth
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware]; 
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean); 
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
-/*Middleware provides a way to extend Redux with custom functionality. It 
-can intercept actions before they reach the reducer, allowing you to add 
-features like logging, crash reporting, or asynchronous API calls. In this 
-code, redux-logger is being used as middleware to log state changes. */
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-export const store = createStore(persistReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
 export const persistor = persistStore(store);
